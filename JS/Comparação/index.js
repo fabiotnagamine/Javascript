375378cf02a4db9b92b4f98c4464bfc4b9d331fd
fabiotnagamine <84332616+fabiotnagamine@users.noreply.github.com>
2022-07-21 18:15:53 -0300

    Operadores Lógicos

--- JS/Comparação/index.js
+++ JS/Comparação/index.js
@@ -10,12 +10,12 @@ Operadores de comparação
 !== diferente estrito (verifica valor e tipo)
 */
 
-// const comp = 10 < 100;
-// console.log(comp);
-// const numero1 = '1';
-// const numero2 = 1;
-// console.log("Comparando valores:", numero1 == numero2); 
-// console.log(`Comparando o tipo do valor: ${numero1 === numero2}`);
+const comp = 10 < 100;
+console.log(comp);
+const numero1 = '1';
+const numero2 = 1;
+console.log("Comparando valores:", numero1 == numero2); 
+console.log(`Comparando o tipo do valor: ${numero1 === numero2}`);
 
 /*
 Operadores lógicos 
@@ -25,23 +25,22 @@ Operadores lógicos
 */
 
 
-// let chovendo = true;
-// let caminhada = null;
-// chovendo = false;
-// if (chovendo == true){
-//     caminhada  = false;
-// }else {
-//     caminhada = true 
-// }
+let chovendo = true;
+let caminhada = null;
+chovendo = false;
+if (chovendo == true){
+    caminhada  = false;
+}else {
+    caminhada = true 
+}
 
-// console.log(caminhada)
+console.log(caminhada)
 
 
-// let chovendo= true;
-// let sair = true || false;
+let sair = true || false;
 
-// if (chovendo == true){
-//     sair;
-// }
-// console.log(sair)
+if (chovendo == true){
+    sair;
+}
+ console.log(sair)
 
